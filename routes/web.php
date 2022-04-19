abbe28cdb0ecf915116adfe9e1f8ff929f576f93
Łukasz Radziak <lukasilestam@gmail.com>
2022-04-19 23:28:55 +0200

    Install Tailwind

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'index')->name('index');
 
 Route::resource('client', ClientController::class);
